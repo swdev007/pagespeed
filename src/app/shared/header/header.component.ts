811c4cb4868ebe1c6f602d4f05dafd4d4c9bfb48
Paras Mogla <parasmogla01644@gmail.com>
2024-09-05 14:05:51 +0530

    assets added to angular.json

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,8 +5,69 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  menu: any[] = [
+    {
+      label: 'Coverage Plans',
+      link: '',
+      children: [
+        {
+          label: 'Our Plans',
+          link: '/plans',
+        },
+        {
+          label: 'Breakdown Protection',
+          link: 'plans/extended-car-warranty',
+        },
+        {
+          label: 'Diamond Coverage',
+          link: '/plans/extended-car-warranty/diamond-coverage',
+        },
+        {
+          label: 'Elite Coverage',
+          link: '/plans/extended-car-warranty/elite-coverage',
+        },
+        {
+          label: 'Premier Coverage',
+          link: '/plans/extended-car-warranty/premier-coverage',
+        },
+        {
+          label: 'Powertrain Coverage',
+          link: '/plans/extended-car-warranty/powertrain-coverage',
+        },
+        {
+          label: 'EV Coverage',
+          link: '/plans/extended-car-warranty/ev-coverage',
+        },
+      ],
+    },
+    {
+      label: 'Help & Support',
+      link: '',
+      children: [
+        {
+          label: 'Contact Us',
+          link: '',
+        },
+        {
+          label: 'About us',
+          link: '',
+        },
+        {
+          label: 'Why NobleQuote',
+          link: '',
+        },
+        {
+          label: 'Opt-Out',
+          link: '',
+        },
+      ],
+    },
+    {
+      label: 'Resources',
+      link: '',
+    },
+  ];
 }
