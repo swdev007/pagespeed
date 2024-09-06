67291eb03ca9de15f3ea5bddacd509e384ac5328
Paras Mogla <parasmogla01644@gmail.com>
2024-09-06 15:21:22 +0530

    before banner  section

--- src/app/shared/components/banner-with-form/banner-with-form.component.ts
+++ src/app/shared/components/banner-with-form/banner-with-form.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, inject, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner-with-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './banner-with-form.component.html',
-  styleUrl: './banner-with-form.component.scss'
+  styleUrl: './banner-with-form.component.scss',
 })
 export class BannerWithFormComponent {
+  destroyRef = inject(DestroyRef);
 
+  @Input() caption = '';
+  @Input() description = '';
+  @Input() image = '';
 }
